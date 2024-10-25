3937b5f966bf4ec9b5bc0b2e4ebaa90f19388b4f
Rohan Kumar Gupta <116427952+Rohan-code-maker@users.noreply.github.com>
2024-10-25 10:15:26 +0530

    added user controller and user router

--- youtube/src/app.js
+++ youtube/src/app.js
@@ -14,8 +14,9 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-app.get("/", (req, res) => {
-    res.send("Hello, World!")
-})
+//routes import
+import userRouter from "./routes/user.routes.js"
+//router declaration
+app.use("/api/v1/users",userRouter)
 
 export {app}
